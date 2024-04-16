bc64c8f0bfdfa6c0ac94d646736f79be2c562305
jucerlina <167243763+jucerlina@users.noreply.github.com>
2024-04-16 16:47:14 -0300

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,17 @@
+peso=float(input("digite seu peso:")) 
+altura= float(input("digite sua altura:"))
+imc =peso/(altura*altura)
+
+if imc= <18.5: 
+    print("magreza")
+elif imc >18.5 and imc < 24.9:
+    print("saudável")
+elif imc > 25 and imc < 29.9:
+     print( "sobrepeso")
+elif imc >30 and imc < 34.9: 
+      print(" obesidade grau 1")
+elif imc > 35 and imc < 39.9
+     print(" obesidade grau 2")
+       
+else:
+     print ( "obeso morbido")
